68c14a6572201c84b40c0d6a376a840ccf470e31
Michael Reitgruber <reitgruber.michael@gmail.com>
2014-10-13 14:05:48 +0200

    adds flood fill functionality

--- src/AsciiShop.java
+++ src/AsciiShop.java
@@ -8,23 +8,91 @@ public class AsciiShop {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int width = 0;
+        int width = -1;
         int height= 0;
-        if(scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-            width = input.length();             //get length of first line for later comparison
-            height++;
-        }
-        while(scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            if(line.length()!=width) {          //print INPUT MISMATCH and return if length is not equal
+        String[] image = null;
+        if(scanner.hasNext()) {
+            if(!scanner.next().equals("read")) {
                 System.out.println("INPUT MISMATCH");
                 return;
             }
-            height++;
+            if(!scanner.hasNextInt()) {
+                System.out.println("INPUT MISMATCH");
+                return;
+            }
+            height = scanner.nextInt();
         }
 
+        image = new String[height];
+
+        for(int line=0; line<height; line++) {
+            if(!scanner.hasNext()) {
+                System.out.println("INPUT MISMATCH");
+                return;
+            }
+            String input = scanner.next();
+            if(width<0) {
+                width = input.length();
+            }
+
+            if(input.length() != width) {
+                System.out.println("INPUT MISMATCH");
+                return;
+            }
+            image[line] = input;
+        }
+
+        while(scanner.hasNext()) {
+            String command = scanner.next();
+            if(command.equals("fill")) {
+                if(!scanner.hasNextInt()) {
+                    System.out.println("INPUT MISMATCH");
+                    return;
+                }
+                int x = scanner.nextInt();
+                if(!scanner.hasNextInt()) {
+                    System.out.println("INPUT MISMATCH");
+                    return;
+                }
+                int y = scanner.nextInt();
+                if(!scanner.hasNext()) {
+                    System.out.println("INPUT MISMATCH");
+                    return;
+                }
+                char c = scanner.next().charAt(0);
+                if(x > width || x < 0 || y > height || y < 0) {
+                    System.out.println("OPERATION FAILED");
+                    return;
+                }
+                fill(image,x,y,c);
+            }
+            else {
+                System.out.println("INPUT MISMATCH");
+                return;
+            }
+        }
+
+        for(String l:image) {
+            System.out.println(l);
+        }
         System.out.println(width + " " + height);
     }
 
+    public static void fill(String[] image, int x, int y, char c) {
+        char replace = image[y].charAt(x);
+        String newLine = image[y].substring(0,x)+c+image[y].substring(x+1);
+        image[y] = newLine;
+        if(x-1 >= 0 && image[y].charAt(x-1) == replace) {
+            fill(image,x-1,y,c);
+        }
+        if(x+1 < image[y].length() && image[y].charAt(x+1) == replace) {
+            fill(image,x+1,y,c);
+        }
+        if(y-1 >=0 && image[y-1].charAt(x) == replace) {
+            fill(image,x,y-1,c);
+        }
+        if(y+1 < image.length && image[y+1].charAt(x) == replace) {
+            fill(image,x,y+1,c);
+        }
+    }
 }
